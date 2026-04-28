f03324eeeac9e6aec2d0cf68405a71c3e4fbf73a
Graham--M <Graham--M@users.noreply.github.com>
2026-04-28 17:00:10 +0100

    Build with Compiler Optimizations

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 # information embedded in it that allows its exports to be loaded by Windows
 # code. It will feed the '.spec' file to the confusingly named 'winebuild' tool
 # which generates some assembly source code with the needed information.
-winegcc -o XWinTabHelper.dll.so -shared  src/XWinTabHelper.c src/XWinTabHelper.dll.spec -lxcb -lxcb-xinput
+winegcc -o XWinTabHelper.dll.so -shared -O2 src/XWinTabHelper.c src/XWinTabHelper.dll.spec -lxcb -lxcb-xinput
 
 # The actual wintab DLL is written as a Windows DLL to avoid relying on
 # any wine interals. Therefore you also need the mingw cross compiler.
-x86_64-w64-mingw32-gcc -shared -o wintab32.dll src/WinTab.c
+x86_64-w64-mingw32-gcc -shared -O2 -o wintab32.dll src/WinTab.c
